600c3208d3c6e9708835d26008d7ae181b8127fa
rbenavidesa <rbenavidesaguila@gmail.com>
2022-03-02 01:55:04 -0300

    3rd commit

---
+++ desafio3/serverExpress.js
@@ -0,0 +1,34 @@
+const Contenedor = require('./contenedor.js');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+let container;
+
+const server = app.listen(PORT, async () => {
+	// Se ejecuta el constructor de la clase
+	container = new Contenedor('productos.txt');
+
+	// Se utilizó un método adicional que inicializa el contador del último id registrado. Se optó por no llamar esta función de constructor ya que es una función asincrona
+	await container.init();
+
+	console.log('👽 Servidor escuchando en el puerto http://localhost:' + PORT);
+});
+
+server.on('error', (error) => console.log(error));
+
+app.get('/productos', async (req, res) => {
+	// A continuación se prueba la función getAll()
+	let products = await container.getAll();
+
+	res.json(products);
+});
+
+app.get('/productoRandom', async (req, res) => {
+	// A continuación se prueba la función getAll()
+	let products = await container.getAll();
+	const product = products[Math.floor(Math.random() * products.length)];
+
+	res.json(product);
+});
